e671698f83fb526317ebe8550094133575a74330
Duncan Ewan <24977320+ThatOneNeji@users.noreply.github.com>
2021-01-24 10:30:06 +0200

    Added base files

---
+++ index.js
@@ -0,0 +1,8 @@
+const libLogger = require('./lib/logger.js');
+module.exports = libLogger;
+
+const libMessageBroker = require('./lib/messagebroker.js');
+module.exports = libMessageBroker;
+
+const libNejiUtils = require('./lib/nejiutils.js');
+module.exports = libNejiUtils;
