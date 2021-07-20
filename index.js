ade1fdf3047f9613c3f1e8273ae33cd61fd2f84a
ThatOneNeji <dev.duncane@users.noreply.github.com>
2021-07-20 13:14:24 +0200

    Bump to 1.6.1
    Updated libs
    Some minor comment cleanup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /**
  * @file
  * @author ThatOneNeji
- * @version 1.6.0
+ * @version 1.6.1
  * @overview
  * <p>This group of classes provide functionality that is reused across the various netstats applications.</p><br>
  * <hr style="margin-top: 1px; margin-bottom: 1px;">
@@ -13,22 +13,19 @@
  * <h2 style="margin-top: 2px;">Using</h2>
  * <h4>Logger</h4>
  * <p>The <i>Logger</i> class is used for logging.</p>
- * <h6>Declaring</h6>
- * <pre>const Logger = require('commonfunctions').Logger;</pre><br>
- * <h6>Using</h6>
- * <pre>let Logging = new Logger();</pre><br>
+ * <h6>Declaring and Using</h6>
+ * <pre>const Logger = require('commonfunctions').Logger;
+ * const Logging = new Logger();</pre><br>
  * <h4>MessageBroker</h4>
  * <p>The <i>MessageBroker</i> class handles the incoming and outgoing messages.</p>
- * <h6>Declaring</h6>
- * <pre>const MessageBroker = require('commonfunctions').MessageBroker;</pre><br>
- * <h6>Using</h6>
- * <pre>let MessageBroker = new MessageBroker();</pre><br>
+ * <h6>Declaring and Using</h6>
+ * <pre>const Messagebroker = require('commonfunctions').MessageBroker;
+ * const MessageBroker = new Messagebroker();</pre><br>
  * <h4>NejiUtils</h4>
  * <p>The <i>NejiUtils</i> class contains commonly used functions.</p>
- * <h6>Declaring</h6>
- * <pre>const NejiUtils = require('commonfunctions').NejiUtils;</pre><br>
- * <h6>Using</h6>
- * <pre>let NejiUtils = new NejiUtils();</pre><br>
+ * <h6>Declaring and Using</h6>
+ * <pre>const Nejiutils = require('commonfunctions').NejiUtils;
+ * const NejiUtils = new Nejiutils();</pre><br>
  */
 
 
